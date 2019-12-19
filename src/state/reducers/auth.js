dbfc027f457c5cbb9dacbd8d6cec914bb590d67b
ofega <fegaeze@gmail.com>
2019-12-19 23:12:40 +0100

    feat(dashboard): pulled reviewList components

--- src/state/reducers/auth.js
+++ src/state/reducers/auth.js
@@ -1,26 +1,25 @@
 import * as types from '../types';
 
-
 const initialState = {
   isLoggingIn: false,
-  isLoggedIn: false
-}
+  isLoggedIn: false,
+};
 
 export const authState = (state = initialState, action) => {
-    switch(action.type) {
-        case types.LOG_IN_USER:
-            return {
-              ...state,
-              isLoggingIn: true
-            };
-        case types.GET_COMPANY_REVIEWS_SUCCESS:
-            return {
-              ...state,
-              isLoggedIn: true
-            }
-      case types.GET_COMPANY_REVIEWS_FAILURE:
-          return state;
-        default:
-            return state
-    }
-}
+  switch (action.type) {
+    case types.LOG_IN_USER:
+      return {
+        ...state,
+        isLoggingIn: true,
+      };
+    case types.GET_COMPANY_REVIEWS_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+      };
+    case types.GET_COMPANY_REVIEWS_FAILURE:
+      return state;
+    default:
+      return state;
+  }
+};
