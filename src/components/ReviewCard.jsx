6de77400b7bd8d71e511affaf5c9a011f24b5168
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-12-19 16:59:56 +0000

    ft(dashboard): dummy review data with demonstration rating stars added to dash for demo purposes

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Card, Rate } from 'antd';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+  margin: 10px;
+`;
+
+const StyledH3 = styled.h3`
+  font-family: Roboto;
+`;
+
+export default function ReviewCard(props) {
+  const { text, name } = props;
+  console.log(props);
+  return (
+    <StyledDiv>
+      <Card style={{ width: 400, height: 280 }}>
+        <div>
+          <StyledH3>Company Name: {name}</StyledH3>
+          <p>{text}</p>
+          <Rate disabled defaultValue={4} />
+        </div>
+      </Card>
+    </StyledDiv>
+  );
+}
--- src/state/actions/reviews.js
+++ src/state/actions/reviews.js
@@ -1,69 +1,59 @@
 import axios from 'axios';
 import * as types from '../types';
 
-
 export const getCompanyReviews = () => async dispatch => {
+  dispatch({
+    type: types.GET_COMPANY_REVIEWS,
+  });
 
+  try {
+    const response = await axios.get('/');
     dispatch({
-        type: types.GET_COMPANY_REVIEWS,
-    })
-
-    try {
-        const response = await axios.get("/");
-        dispatch({
-            type: types.GET_COMPANY_REVIEWS_SUCCESS,
-            payload: response.data
-        })
-
-    } catch (error) {
-        dispatch({
-            type: types.GET_COMPANY_REVIEWS_FAILURE,
-            payload: error.message || 'Something went wrong.'
-        })
-    }
-
-}
+      type: types.GET_COMPANY_REVIEWS_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: types.GET_COMPANY_REVIEWS_FAILURE,
+      payload: error.message || 'Something went wrong.',
+    });
+  }
+};
 
 export const getSalaryReviews = () => async dispatch => {
+  dispatch({
+    type: types.GET_SALARY_REVIEWS,
+  });
 
+  try {
+    const response = await axios.get('/');
     dispatch({
-        type: types.GET_SALARY_REVIEWS,
-    })
-
-    try {
-        const response = await axios.get("/");
-        dispatch({
-            type: types.GET_SALARY_REVIEWS_SUCCESS,
-            payload: response.data
-        })
-
-    } catch (error) {
-        dispatch({
-            type: types.GET_SALARY_REVIEWS_FAILURE,
-            payload: error.message || 'Something went wrong.'
-        })
-    }
-
-}
+      type: types.GET_SALARY_REVIEWS_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: types.GET_SALARY_REVIEWS_FAILURE,
+      payload: error.message || 'Something went wrong.',
+    });
+  }
+};
 
 export const getInterviewReviews = () => async dispatch => {
+  dispatch({
+    type: types.GET_INTERVIEW_REVIEWS,
+  });
 
+  try {
+    const response = await axios.get('/');
     dispatch({
-        type: types.GET_INTERVIEW_REVIEWS,
-    })
-
-    try {
-        const response = await axios.get("/");
-        dispatch({
-            type: types.GET_INTERVIEW_REVIEWS_SUCCESS,
-            payload: response.data
-        })
-
-    } catch (error) {
-        dispatch({
-            type: types.GET_INTERVIEW_REVIEWS_FAILURE,
-            payload: error.message || 'Something went wrong.'
-        })
-    }
-
-}
+      type: types.GET_INTERVIEW_REVIEWS_SUCCESS,
+      payload: response.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: types.GET_INTERVIEW_REVIEWS_FAILURE,
+      payload: error.message || 'Something went wrong.',
+    });
+  }
+};
