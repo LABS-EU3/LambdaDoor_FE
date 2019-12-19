667fd818ef76586611ef49c1d36776f59d1546fd
ofega <fegaeze@gmail.com>
2019-12-19 15:09:42 +0100

    pulled merged data

---
+++ src/state/reducers/auth.js
@@ -0,0 +1,26 @@
+import * as types from '../types';
+
+
+const initialState = {
+  isLoggingIn: false,
+  isLoggedIn: false
+}
+
+export const authState = (state = initialState, action) => {
+    switch(action.type) {
+        case types.LOG_IN_USER:
+            return {
+              ...state,
+              isLoggingIn: true
+            };
+        case types.GET_COMPANY_REVIEWS_SUCCESS:
+            return {
+              ...state,
+              isLoggedIn: true
+            }
+      case types.GET_COMPANY_REVIEWS_FAILURE:
+          return state;
+        default:
+            return state
+    }
+}
---
+++ src/state/reducers/reviews.js
@@ -0,0 +1,73 @@
+import * as types from '../types';
+
+const initialState = {
+  isFetching: false,
+  reviews: {
+    company: [],
+    salary: [],
+    interview: []
+  }
+}
+
+export const reviewsReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case types.GET_COMPANY_REVIEWS:
+    case types.GET_SALARY_REVIEWS:
+    case types.GET_INTERVIEW_REVIEWS:
+        return {...state, isFetching: true };
+
+    case types.GET_COMPANY_REVIEWS_SUCCESS:
+        return {
+          ...state,
+          reviews: {
+            ...state.reviews,
+            company: [...state.reviews.company, action.payload]
+          }
+        };
+
+    case types.GET_SALARY_REVIEWS_SUCCESS:
+        return {
+          ...state,
+          reviews: {
+            ...state.reviews,
+            salary: [...state.reviews.salary, action.payload]
+          }
+        };
+
+    case types.GET_INTERVIEW_REVIEWS_SUCCESS:
+        return {
+          ...state,
+          reviews: {
+            ...state.reviews,
+            salary: [...state.reviews.salary, action.payload]
+          }
+        };
+
+    case types.GET_COMPANY_REVIEWS_FAILURE:
+    case types.GET_SALARY_REVIEWS_FAILURE:
+    case types.GET_INTERVIEW_REVIEWS_FAILURE:
+      return {...state, isFetching: false};
+
+    default:
+        return state
+  }
+}
+
+export const reviewsError = (state = null, action) => {
+    switch(action.type) {
+        case types.GET_COMPANY_REVIEWS_FAILURE:
+        case types.GET_SALARY_REVIEWS_FAILURE:
+        case types.GET_INTERVIEW_REVIEWS_FAILURE:
+            return action.payload;
+        case types.GET_COMPANY_REVIEWS:
+        case types.GET_SALARY_REVIEWS:
+        case types.GET_INTERVIEW_REVIEWS:
+            return null;
+        case types.GET_COMPANY_REVIEWS_SUCCESS:
+        case types.GET_SALARY_REVIEWS_SUCCESS:
+        case types.GET_INTERVIEW_REVIEWS_SUCCESS:
+            return null;
+        default:
+            return state
+    }
+}
