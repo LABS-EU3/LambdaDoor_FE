cc57f54dee9c3c3f0aac40ed4e993dab4461c411
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-12-20 08:36:20 +0100

    feat(dashboard): capture users current location

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -10,6 +10,42 @@ const StyledH1 = styled.h1`
 `;
 
 const UserDashboard = props => {
+  useEffect(() => {
+    function showPosition(position) {
+      const result = `${position.coords.latitude}  ${position.coords.longitude}`;
+      console.log(result);
+    }
+
+    function showError(error) {
+      switch (error.code) {
+        case error.PERMISSION_DENIED:
+          console.log('User denied the request for Geolocation.');
+          break;
+        case error.POSITION_UNAVAILABLE:
+          console.log('Location information is unavailable.');
+          break;
+        case error.TIMEOUT:
+          console.log('The request to get user location timed out.');
+          break;
+        case error.UNKNOWN_ERROR:
+          console.log('An unknown error occurred.');
+          break;
+        default:
+          return null;
+      }
+      return null;
+    }
+    function getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+      } else {
+        console.log('Geolocation is not supported by this browser.');
+      }
+    }
+
+    getLocation();
+  }, []);
+
   return (
     <div>
       <StyledH1>Latest Reviews</StyledH1>
