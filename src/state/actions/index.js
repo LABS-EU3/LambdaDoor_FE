fb2904d8f81de0c2e9f60f86399a9bc17f10f333
Victor <arowovici@gmail.com>
2019-12-20 08:45:32 +0100

    WIP

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -6,7 +6,6 @@ import {
 } from './reviews';
 
 axios.defaults.baseURL = 'https://lambdadoor-staging.herokuapp.com/';
-axios.defaults.baseURL = 'http://localhost:5000/';
 
 // eslint-disable-next-line import/prefer-default-export
 export const actions = {
