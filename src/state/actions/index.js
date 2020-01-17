43be4be74f30a2d649a96e9a9656ba5864d42925
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-17 11:06:37 +0100

    reducers and actions created

--- src/state/actions/index.js
+++ src/state/actions/index.js
@@ -3,10 +3,12 @@ import {
   getSalaryReviews,
   getInterviewReviews,
 } from './reviews';
+import { getTopRatedReviews } from './topRatedReviews';
 
 // eslint-disable-next-line import/prefer-default-export
 export const actions = {
   getCompanyReviews,
   getSalaryReviews,
   getInterviewReviews,
+  getTopRatedReviews,
 };
--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -2,12 +2,14 @@ import { combineReducers } from 'redux';
 import { reviewsReducer, reviewsError } from './reviews';
 import { authState } from './auth';
 import { interestReducer } from './interests';
+import { topRatedReviewsReducer } from './topRatedReviews';
 
 const rootReducer = combineReducers({
   reviews: reviewsReducer,
   reviewsError: reviewsError,
   authState: authState,
   interests: interestReducer,
+  topRatedReviews: topRatedReviewsReducer,
 });
 
 export default rootReducer;
