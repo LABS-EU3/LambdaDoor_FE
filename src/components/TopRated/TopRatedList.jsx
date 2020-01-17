c7b7f2fbe3c09bf79c68b4c02df4303e70b3f45b
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-17 11:06:53 +0100

    connected component to redux store

--- src/components/TopRated/TopRatedList.jsx
+++ src/components/TopRated/TopRatedList.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { connect } from 'react-redux';
 import TopRatedCard from './TopRatedCard';
+import { getTopRatedReviews } from '../../state/actions/topRatedReviews';
 import { interviewReviews } from '../../utils/data';
 
 const StyledDiv = styled.div`
@@ -9,20 +11,27 @@ const StyledDiv = styled.div`
   max-width: 1400px;
 `;
 
-const ReviewList = () => {
+const TopRatedList = ({
+  getTopRatedReviews,
+  topRatedReviews: { topRatedReviews },
+}) => {
+  useEffect(() => {
+    getTopRatedReviews();
+  }, []);
   return (
     <StyledDiv>
-      {interviewReviews.map(review => (
+      {topRatedReviews.map(topRated => (
         // eslint-disable-next-line react/no-array-index-key
         <TopRatedCard
-          key={`${review.id}${review.user_id}`}
-          text={review.text}
-          name={review.company_name}
-          id={review.id}
+          key={`${topRated.id}`}
+          text={topRated.description || 'Its A beautiful Day to be alive'}
+          name={topRated.name}
+          id={topRated.id}
+          rating={topRated.average_rating}
         />
       ))}
     </StyledDiv>
   );
 };
 
-export default ReviewList;
+export default connect(state => state, { getTopRatedReviews })(TopRatedList);
