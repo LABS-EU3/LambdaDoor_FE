4032a36deed1efaa45d0a7e7170560640e6bd3fe
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-21 13:31:46 +0100

    conflicts resolved

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -2,26 +2,20 @@ import { combineReducers } from 'redux';
 import { reviewsReducer, reviewsError } from './reviews';
 import { authState } from './auth';
 import { interestReducer } from './interests';
-<<<<<<< HEAD
 import { topRatedReviewsReducer } from './topRatedReviews';
 import { closestCompaniesReducer } from './closestCompanies'
-=======
 import { companiesReducer } from './companies';
 import { jobrolesReducer } from './jobroles';
->>>>>>> 04d95cfcf58043664a06a2f8e3a6cc924d926bc8
 
 const rootReducer = combineReducers({
   reviews: reviewsReducer,
   reviewsError: reviewsError,
   authState: authState,
   interests: interestReducer,
-<<<<<<< HEAD
   topRatedReviews: topRatedReviewsReducer,
   closestCompanies: closestCompaniesReducer,
-=======
   companies: companiesReducer,
   jobroles: jobrolesReducer,
->>>>>>> 04d95cfcf58043664a06a2f8e3a6cc924d926bc8
 });
 
 export default rootReducer;
