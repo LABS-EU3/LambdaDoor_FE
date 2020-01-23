05a8a99bd68df33dcaeebe94363293d9811d8907
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-23 11:31:32 +0100

    added style to enable link clickable

--- src/components/UserLocationComp/ClosestLocationCard.jsx
+++ src/components/UserLocationComp/ClosestLocationCard.jsx
@@ -3,6 +3,14 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Website = styled.a`
+  position: absolute;
+  top: 113px;
+  left: 50px;
+  z-index: 2;
+`;
 
 export default function ClosestLocationCard(props) {
   const { text, name, id, website } = props;
@@ -13,13 +21,11 @@ export default function ClosestLocationCard(props) {
           <h3>{name}</h3>
           <p>{text}</p>
         </div>
-        <p style={{ marginBottom: 0 }}>
-          Link:{' '}
-          <a href={website} target="_blank" rel="noopener noreferrer">
-            {website}
-          </a>
-        </p>
+        <p style={{ marginBottom: 0 }}>Link: </p>
       </Link>
+      <Website href={website} target="_blank" rel="noopener noreferrer">
+        {website}
+      </Website>
     </div>
   );
 }
