91bd28e598e50730e09dc94f4c3ac706d467cc47
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-01-23 10:35:11 +0100

    changed  ID to be dynamic

--- src/state/actions/closestCompanies.js
+++ src/state/actions/closestCompanies.js
@@ -2,16 +2,16 @@
 import axios from 'axios';
 import * as types from '../types';
 
-export const getClosestCompanies = req => async dispatch => {
-//   const { id } = req.params;
+export const getClosestCompanies = () => async (dispatch, getState) => {
   dispatch({
     type: types.GET_CLOSEST_COMPANIES,
   });
 
   try {
     const response = await axios.get(
-      //I think this ID should be changed to be dynamic for users rather than be hardcoded to '2'
-      `${process.env.REACT_APP_BACKEND_URL}/companies/2/closest`
+      `${process.env.REACT_APP_BACKEND_URL}/companies/${
+        getState().authState.credentials.id
+      }/closest`
     );
     dispatch({
       type: types.GET_CLOSEST_COMPANIES_SUCCESS,
