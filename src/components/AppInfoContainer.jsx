cfaa70fc71b3f7c291475755add834fc771efc2f
Lisa Wilton <mishmao14@gmail.com>
2020-01-30 15:43:52 +0000

    initial section for app-info created

--- src/components/AppInfoContainer.jsx
+++ src/components/AppInfoContainer.jsx
@@ -2,28 +2,83 @@ import React from 'react';
 import { Icon } from 'antd';
 import styled from 'styled-components';
 
-function AppInfoContainer() {
+function AppInfoContainer(props) {
   return (
-    <div>
-      <h2>Features</h2>
-      <p>
-        No need to sign up: Lambda students have automatic access. Just sign in
-        via Slack.
-      </p>
-      <p>
-        Leave company reviews, salary reviews and interview reviews publicly or
-        anonymously.
-      </p>
-      <p>
-        Access up to date information about companies in your area or around the
-        world.
-      </p>
-      <p>
-        Email referrals: contact fellow students at the click of a button with
-        questions about the companies you're interested in.
-      </p>
-    </div>
+    <StyledDiv infoVisible={props.infoVisible}>
+      <TextBox>
+        <div className="heading">
+          <h2>Features</h2>
+          <Icon type="arrow-up" className="arrow-up" onClick={props.hideInfo} />
+        </div>
+        <InfoBox>
+          <p className="left-outer">
+            No need to sign up: Lambda students have automatic access. Just sign
+            in via Slack.
+          </p>
+          <p className="left-center">
+            Leave company reviews, salary reviews and interview reviews publicly
+            or anonymously.
+          </p>
+          <p className="right-center">
+            Access up to date information about companies in your area or around
+            the world.
+          </p>
+          <p className="right-outer">
+            Email referrals: contact fellow students at the click of a button
+            with questions about the companies you're interested in.
+          </p>
+        </InfoBox>
+      </TextBox>
+    </StyledDiv>
   );
 }
 
 export default AppInfoContainer;
+
+const StyledDiv = styled.div`
+  position: absolute;
+  background-color: #bb1333;
+  transition: all 0.2s ease-in-out;
+  top: ${props => (props.infoVisible ? `0%` : `100%`)};
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TextBox = styled.div`
+  /* margin: 50px auto; */
+  width: 80%;
+  height: 70%;
+  background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 4rem;
+  h2 {
+    font-size: 35px;
+    font-family: 'Roboto';
+    margin-bottom: 0;
+  }
+  .heading {
+    width: 150px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    .arrow-up {
+      font-size: 30px;
+    }
+  }
+`;
+
+const InfoBox = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 2rem;
+  p {
+    width: 25%;
+    padding: 1rem;
+    font-size: 18px;
+  }
+`;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -25,6 +25,14 @@ export const Home = ({ history, SetAuthenticated }) => {
   const [error, setError] = useState(null);
   const [infoVisible, setInfoVisible] = useState(false);
 
+  const viewInfo = () => {
+    setInfoVisible(true);
+  };
+
+  const hideInfo = () => {
+    setInfoVisible(false);
+  };
+
   useEffect(() => {
     const token = localStorage.getItem('token');
     if (token) {
@@ -35,67 +43,73 @@ export const Home = ({ history, SetAuthenticated }) => {
   }, [history, SetAuthenticated]);
 
   return (
-    <HomeContainer>
-      {/* <AppInfoContainer infoVisible={infoVisible} /> */}
-      <HomeContentContainer>
-        <Logo />
+    <div>
+      <HomeContainer infoVisible={infoVisible}>
+        <HomeContentContainer>
+          <Logo />
 
-        <OnboardingContainer>
-          <Title className="siteTitle">Lambda Door</Title>
+          <OnboardingContainer>
+            <Title className="siteTitle">Lambda Door</Title>
 
-          <Paragraph>
-            The one-stop portal for Lambda graduates looking for company
-            information in the quest for a job.
+            <Paragraph>
+              The one-stop portal for Lambda graduates looking for company
+              information in the quest for a job.
+            </Paragraph>
+            <Icon type="arrow-down" onClick={viewInfo} />
+
+            <a
+              href={`https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`}
+            >
+              <img
+                alt="Sign in with Slack"
+                height="40"
+                width="172"
+                src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
+                srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
+              />
+            </a>
+            {error && (
+              <Alert
+                message="An error occured while signing in!!"
+                type="error"
+                showIcon
+                closable
+                style={{
+                  fontSize: '16px',
+                  marginTop: '10px',
+                  width: '100%',
+                  maxWidth: '300px',
+                }}
+                onClose={() => setError(null)}
+              />
+            )}
+          </OnboardingContainer>
+
+          <Paragraph style={{ color: 'white' }}>
+            Built by Lambda students, for Lambda students.
           </Paragraph>
-          {/* <Icon type="arrow-down" onClick={setInfoVisible(true)} /> */}
-
-          <a
-            href={`https://slack.com/oauth/authorize?scope=identity.basic,identity.email,identity.team,identity.avatar&client_id=${process.env.REACT_APP_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`}
-          >
-            <img
-              alt="Sign in with Slack"
-              height="40"
-              width="172"
-              src="https://platform.slack-edge.com/img/sign_in_with_slack.png"
-              srcSet="https://platform.slack-edge.com/img/sign_in_with_slack.png 1x, https://platform.slack-edge.com/img/sign_in_with_slack@2x.png 2x"
-            />
-          </a>
-          {error && (
-            <Alert
-              message="An error occured while signing in!!"
-              type="error"
-              showIcon
-              closable
-              style={{
-                fontSize: '16px',
-                marginTop: '10px',
-                width: '100%',
-                maxWidth: '300px',
-              }}
-              onClose={() => setError(null)}
-            />
-          )}
-        </OnboardingContainer>
-
-        <Paragraph style={{ color: 'white' }}>
-          Built by Lambda students, for Lambda students.
-        </Paragraph>
-      </HomeContentContainer>
-    </HomeContainer>
+        </HomeContentContainer>
+      </HomeContainer>
+      <AppInfoContainer infoVisible={infoVisible} hideInfo={hideInfo} />
+    </div>
   );
 };
 
 export default connect(null, { LoginUser, SetAuthenticated })(Home);
 
+const StyledContainer = styled.div`
+   
+`;
+
 const HomeContainer = styled.div`
   background-image: url(${background});
   background-size: cover;
   background-position: top right;
-  background-attachment: fixed;
+  /* background-attachment: fixed; */
   background-repeat: no-repeat;
   width: 100vw;
   height: 100vh;
-
+  /* position: absolute; */
   @media ${tabletPortrait} {
     background-position: -180px 50px;
   }
